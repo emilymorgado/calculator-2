0a8579adac7871ccc26aecf88324d98e2c50cdfa
Hackbright Student <info@hackbrightacademy.com>
2015-09-23 17:57:58 -0700

    Added Error Messages

--- calculator.py
+++ calculator.py
@@ -9,34 +9,53 @@ from arithmetic import *
 
 
 # Your code goes here
+
 while True:
-    calc = raw_input("> ")
+    try:
+        calc = raw_input("> ")
 
-    tokens = calc.split(" ")
+        tokens = calc.split(" ")
 
-    x = int(tokens[1])
-    y = int(tokens[2])
+        x = int(tokens[1])
+        y = int(tokens[2])
 
-    if tokens[0] == "+":
-        print add(x, y)
+        if tokens[0] == "+":
+            print add(x, y)
+            continue
 
-    if tokens[0] == "-":
-        print subtract(x, y)
+        if tokens[0] == "-":
+            print subtract(x, y)
+            continue
 
-    if tokens[0] == "*":
-        print multiply(x, y)
+        if tokens[0] == "*":
+            print multiply(x, y)
+            continue
 
-    if tokens[0] == "/":
-        print divide(x, y)
+        if tokens[0] == "/":
+            print divide(x, y)
+            continue
 
-    if tokens[0] == "square":
-        print square(x)
+        if tokens[0] == "square":
+            print square(x)
+            continue
 
-    if tokens[0] == "cube":
-        print cube(x)
+        if tokens[0] == "cube":
+            print cube(x)
+            continue
 
-    if tokens[0] == "pow":
-        print power(x, y)
+        if tokens[0] == "pow":
+            print power(x, y)
+            continue
 
-    if tokens[0] == "mod":
-        print mod(x, y)
+        if tokens[0] == "mod":
+            print mod(x, y)
+            continue
+
+        if tokens[0] == "q" or "Q":
+            break
+
+    except ValueError:
+        print "I don't understand. Please try again."
+
+    except IndexError:
+        print "Please enter three values."
